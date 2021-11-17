05fa1d7f5f7623c5b9cc0044221442e5ac792a9f
eliaouAzulai <eliaou.re@gmail.com>
2021-11-17 09:46:29 +0200

    password-checker

---
+++ checkmypass.py
@@ -0,0 +1,50 @@
+# type into the command line a passord
+# check that password has ever been hacked
+# reply with a string if found or not found
+
+# add reading from a txt file since the command line saves the last entry
+
+
+import requests
+import hashlib
+import sys
+
+def request_api_data(query_char):
+    url = 'https://api.pwnedpasswords.com/range/' + query_char
+    res = requests.get(url)
+    #print(res)
+    if res.status_code != 200:
+        raise RuntimeError(f'Error fetching: {res.status_code} check the api and try again ')
+    return res
+
+def read_res(response):
+    print(response.text)
+
+def get_password_leaks_count(hashes, hash_to_check):
+    hashes = (line.split(':') for line in hashes.text.splitlines())
+    for h, count in hashes:
+        if h == hash_to_check:
+            return count
+    return 0
+
+    
+
+def pwned_api_check(password):
+    sha1password = hashlib.sha1(password.encode('utf-8')).hexdigest().upper()
+    first5_char, tail = sha1password[:5], sha1password[5:]
+    response = request_api_data(first5_char)
+    return get_password_leaks_count(response, tail)
+
+def main(args):
+    for password in args:
+        count = pwned_api_check(password)
+        if count:
+            print(f'{password} was found {count} times.... you should probaly change yuour password')
+        else:
+            print(f'{password} was NOT fount. Carry on !')
+    return 'done!'
+
+if __name__ == '__main__':
+    sys.exit(main(sys.argv[1:]))
+
+
